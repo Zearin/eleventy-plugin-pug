6ac47e30e1e1a4250d9af85a23d8f9649f3690b5
Zearin <Zearin@users.noreply.github.com>
2024-01-17 19:26:32 -0500

    test,doc: Writing notes to myself

--- test/test-minimal-setup.js
+++ test/test-minimal-setup.js
@@ -11,6 +11,14 @@ import {
 import eleventy from '@11ty/eleventy'
 import plugin from '../index.js'
 
+//	Other plugins I’ve looked at test their own code,
+//	but they don’t test how it interacts with Eleventy.
+//
+//	Pug had some a few rough edges in Eleventy 2, so
+//	I want to be sure it’s solid for Eleventy 3.
+//
+//	What’s the best way to test how the plugin behaves
+//	when in use by Eleventy…?
 
 describe.todo('SCENARIO: Minimal possible setup', function() {})
 
